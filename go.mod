20e822e4f70643ea3fcd56aadd18c6f0fcae7fb1
Kevin <kevinnethan2008@gmail.com>
2026-07-22 14:28:51 +0800

    2nd Commit Stage 3

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kevinnethan2008/gitwrapped
+
+go 1.24.4
--- main.go
+++ main.go
@@ -3,12 +3,59 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strings"
+	"time"
 
 )
 
+type Commit struct {
+	Hash string
+	Author string
+	Date time.Time
+	Message string
+
+}
+
+func logger(Output string) []Commit{
+	lines := strings.Split(strings.TrimSpace(Output), "\n")
+	commits := make([]Commit,0,len(lines))
+
+	for _, line := range lines{
+	if line == "" {
+	continue
+	}
+
+	parts := strings.SplitN(line,"|",4)
+	if len(parts) != 4 {
+	fmt.Println("too many substrings")
+	continue
+	}
+
+	date, err := time.Parse("Mon Jan 2 15:04:05 2006 -0700", parts[2])
+	if err != nil {
+		fmt.Println("Date error:", err)
+		continue
+	}
+
+	commit := Commit {
+	Hash: parts[0],
+	Author: parts[1],
+	Date: date,
+	Message: parts[3],
+	}
+	commits = append(commits,commit)
+	fmt.Println(commit)
+	fmt.Println(date.Format("2006-01-02 15:04:05"))
+     }
+	return commits
+}
+
+
+
+
 func main() {
 
-	cmd := exec.Commnad("git", "log", "--pretty=format:%H|%an|%ad|%s")
+	cmd := exec.Command("git", "log", "--pretty=format:%H|%an|%ad|%s")
 
 	output, err := cmd.Output()
 	if err != nil {
@@ -16,8 +63,7 @@ func main() {
 	return
 	}
 
-	fmt.Println(string(output))
-
+	logger(string(output))
 
 }
 
