a80a05585510a9bac8c0d54f4d043e73c464f6c0
Kevin <kevinnethan2008@gmail.com>
2026-07-23 16:04:27 +0800

    2nd Last Commit added svg

--- main.go
+++ main.go
@@ -7,7 +7,19 @@ import (
 	"time"
 	"sort"
 	"strconv"
+	"text/template"
+	"os"
 )
+
+const svgTemplate = `<svg width="400" height="300" xmlns="http://www.w3.org/2000/svg">
+  <rect width="400" height="300" fill="#1a1a2e"/>
+  <text x="20" y="40" font-size="20" fill="white">Your Git Wrapped</text>
+  <text x="20" y="80" font-size="14" fill="#aaa">Total commits: {{.TotalCommits}}</text>
+  <text x="20" y="110" font-size="14" fill="#aaa">Busiest hour: {{.BusiestHour}}:00</text>
+  <text x="20" y="140" font-size="14" fill="#aaa">Longest streak: {{.LongestStreak}} days</text>
+  <text x="20" y="170" font-size="14" fill="#aaa">Most-changed file: {{.TopFile}} ({{.TopFileChurn}} lines)</text>
+</svg>`
+
 
 type Commit struct {
 	Hash string
@@ -20,6 +32,14 @@ type Commit struct {
 type fileStats struct {
 	name string
 	churn int
+}
+
+type cardData struct {
+	TotalCommits int
+	BusiestHour int
+	LongestStreak int
+	TopFile string
+	TopFileChurn int
 }
 
 
@@ -57,7 +77,7 @@ func logger(Output string) []Commit{
 	return commits
 }
 
-func bucket(Commit []Commit) {
+func bucket(Commit []Commit) (int,int) {
 
 	hourCount := make(map[int]int)
 	weekdayCounts := make(map[time.Weekday]int)
@@ -111,9 +131,18 @@ func bucket(Commit []Commit) {
 		}
 	}
 	fmt.Println("longest Streak:", longestStreak, "days")
+	BusiestHour := 0
+	maxCount := 0
+	for hour, count := range hourCount {
+		if count > maxCount {
+		maxCount = count
+		BusiestHour = hour
+		}
+	}
+	return longestStreak, BusiestHour
 }
 
-func churn(Output string) {
+func churn(Output string) (string,int) {
 
 fileChurn := make(map[string]int)
 lines := strings.Split((Output), "\n")
@@ -150,6 +179,7 @@ var stats []fileStats
 	for i:=0; i<5 && i<len(stats); i++ {
 	fmt.Printf("%d. %s (%d lines changed)\n", i+1, stats[i].name, stats[i].churn)
 	}
+	return stats[0].name, stats[0].churn
 }
 
 func main() {
@@ -162,7 +192,7 @@ func main() {
 	return
 	}
 
-	bucket(logger(string(output)))
+	longestStreak,busiestHour := bucket(logger(string(output)))
 
 	cmd1 := exec.Command("git", "log", "--numstat", "--pretty=format:COMMIT:%H")
 	output1, err1 := cmd1.Output()
@@ -170,7 +200,37 @@ func main() {
 	fmt.Println("Error running numstat")
 	return
 	}
-	churn(string(output1))
+	name, churn := churn(string(output1))
+	
+	data := cardData {
+		TotalCommits: len(logger(string(output))),
+		BusiestHour: busiestHour,
+		LongestStreak: longestStreak,
+		TopFile: name,
+		TopFileChurn: churn,
+	}
+
+	tmpl, err := template.New("card").Parse(svgTemplate)
+	if err != nil {
+	fmt.Println("Template errror:",err)
+	return
+	}
+
+	file, err := os.Create("wrapped.svg")
+	if err != nil {
+	fmt.Println("File error", err)
+	return
+	}
+	defer file.Close()
+
+	err = tmpl.Execute(file, data)
+	if err != nil {
+	fmt.Println("Execute Errror:", err)
+	return
+	}
+
+	fmt.Println("Wrote wrapped.svg")
+
 
 }
 
