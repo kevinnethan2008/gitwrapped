1fd1b19e3e26b14420f62966d451081168b5d314
Kevin <kevinnethan2008@gmail.com>
2026-07-28 15:50:50 +0800

    testers1

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ const svgTemplate = `<svg width="400" height="{{.CardHeight}}" xmlns="http://www
   </text>
 </svg>`
 
-type Commit struct {
+type Commit struct {	
 	Hash string
 	Author string
 	Date time.Time
